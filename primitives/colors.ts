e719321fa8e2e67bf655ab471208b660a381064c
captainill <jonthomas0@gmail.com>
2020-08-05 21:07:33 -0700

    update colors

--- primitives/colors.ts
+++ primitives/colors.ts
@@ -1,12 +1,11 @@
 export const colors = {
-  "black": "#1b1f23",
+  "black": "#1a1a1a",
   "white": "#fff",
-  "gray": ["#fafbfc", "#f6f8fa", "#e1e4e8", "#d1d5da", "#959da5", "#6a737d", "#586069", "#444d56", "#2f363d", "#24292e"],
-  "blue": ["#f1f8ff", "#dbedff", "#c8e1ff", "#79b8ff", "#2188ff", "#0366d6", "#005cc5", "#044289", "#032f62", "#05264c"],
-  "green": ["#f0fff4", "#dcffe4", "#bef5cb", "#85e89d", "#34d058", "#28a745", "#22863a", "#176f2c", "#165c26", "#144620"],
-  "yellow": ["#fffdef", "#fffbdd", "#fff5b1", "#ffea7f", "#ffdf5d", "#ffd33d", "#f9c513", "#dbab09", "#b08800", "#735c0f"],
-  "orange": ["#fff8f2", "#ffebda", "#ffd1ac", "#ffab70", "#fb8532", "#f66a0a", "#e36209", "#d15704", "#c24e00", "#a04100"],
-  "red": ["#ffeef0", "#ffdce0", "#fdaeb7", "#f97583", "#ea4a5a", "#d73a49", "#cb2431", "#b31d28", "#9e1c23", "#86181d"],
-  "purple": ["#f5f0ff", "#e6dcfd", "#d1bcf9", "#b392f0", "#8a63d2", "#6f42c1", "#5a32a3", "#4c2889", "#3a1d6e", "#29134e"],
-  "pink": ["#ffeef8", "#fedbf0", "#f9b3dd", "#f692ce", "#ec6cb9", "#ea4aaa", "#d03592", "#b93a86", "#99306f", "#6d224f"]
+  "gray": ["#FAFBFC", "#EBEEF2", "#DCE0E6", "#BAC1CC", "#8E96A3", "#6F7682", "#626873", "#525761", "#373A42", "#1F2124"], // 050 - "#F7F8FA"
+  "blue": ["#F2F7FF", "#F0F5FF", "#8AB1FF", "#5B92FF", "#387AFF", "#1563FF", "#0F4FD1", "#0E40A3", "#0A2D74", "#032F62"],
+  "green": ["#ECF7ED", "#C6E9C9", "#A0DBA5", "#7ACC81", "#54BE5D", "#2EB039", "#26912F", "#1E7125", "#15501A", "#1B6203"],
+  "yellow": ["#FFFBED", "#FDEEBA", "#fff5b1", "#FBD95E", "ffdf5d", "#FAC402", "#f9c513", "#A07D02", "#b08800", "#614903"],
+  "orange": ["#FEF4EC", "#FDE0C8", "#FCCCA4", "#FBB77F", "#FAA35B", "#FA8F37", "#CD762E", "#A05C23", "#724119", "#613C12"],
+  "red": ["#F9ECEE", "#EFC7CC", "#E5A2AA", "#DB7D88", "#D15866", "#C73445", "#A32B39", "#7F222C", "#5B1820", "#610B03"],
+  "purple": ["#F4F2FF", "#D5D2F7", "#AEA7F2", "#8D83ED", "#7568E8", "#5C4EE5", "#4C40BC", "#3B3292", "#2A2469", "#1A163F"]
 }
